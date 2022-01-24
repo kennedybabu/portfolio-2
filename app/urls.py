fb637e8b5378bbf1f6b0c60f2c3e40f8fe9b82ef
kennedybabu <kennedynjogu8@gmail.com>
2022-01-24 19:51:19 +0300

    feature:create the about page template, view and url

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,8 @@
+from unicodedata import name
 from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('', views.index, name='home')
+    path('', views.index, name='home'),
+    path('about/', views.about, name='about')
 ]
